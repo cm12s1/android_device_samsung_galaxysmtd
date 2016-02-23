e0239980cb0aa3f83a732c2ea7be7313f00152c2
jeffreyvh <jeffrey25011998@gmail.com>
2016-02-23 12:04:30 +0100

    Update bdroid_buildcfg.h

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "DroidPorn"
+#define BTM_DEF_LOCAL_NAME   "Galaxy S"
 
 #define BLE_INCLUDED TRUE
 #define BTA_GATT_INCLUDED TRUE
